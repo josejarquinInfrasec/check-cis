157233d770872f03840e2842dde790d50489969f
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-17 13:00:16 -0300

    -

---
+++ ubuntu-18.04/3_1_1.sh
@@ -0,0 +1,17 @@
+cis_test_name="Ensure packet redirect sending is disabled (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(sysctl net.ipv4.conf.all.send_redirects)
+	[ "$cmd" != "net.ipv4.conf.all.send_redirects = 0" ] && return 1
+	cmd=$(sysctl net.ipv4.conf.default.send_redirects)
+	[ "$cmd" != "net.ipv4.conf.default.send_redirects = 0" ] && return 1
+	cmd=$(grep "net\.ipv4\.conf\.all\.send_redirects" /etc/sysctl.conf /etc/sysctl.d/*)
+	[ "$cmd" != "net.ipv4.conf.all.send_redirects = 0" ] && return 1
+	cmd=$(grep "net\.ipv4\.conf\.default\.send_redirects" /etc/sysctl.conf /etc/sysctl.d/*)
+	[ "$cmd" != "net.ipv4.conf.default.send_redirects= 0" ] && return 1
+	return 0
+}
