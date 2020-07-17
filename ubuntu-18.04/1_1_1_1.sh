8885c649a21bd6ca4cbe1d069860d59cabbb2a94
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-17 12:41:45 -0300

    -

--- ubuntu-18.04/1_1_1_1.sh
+++ ubuntu-18.04/1_1_1_1.sh
@@ -1,14 +1,13 @@
-kernel_module="cramfs"
-cis_test_name="Ensure mounting of ${kernel_module} filesystems is disabled (Scored)"
+cis_test_name="Ensure mounting of cramfs filesystems is disabled (Scored)"
 cis_test_pa=(server workstation)
 cis_test_spl=1
 cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(modprobe -n -v ${kernel_module} | grep -v mtd)
+	cmd=$(modprobe -n -v cramfs | grep -v mtd)
 	[ "$cmd" != "install /bin/true " ] && return 1
-	cmd=$(lsmod | grep ${kernel_module})
+	cmd=$(lsmod | grep cramfs)
 	[ -n "$cmd" ] && return 1
 	return 0
 }
