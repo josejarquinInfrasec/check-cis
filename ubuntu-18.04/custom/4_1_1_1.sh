e9664d397817a7422e29ea08eac5557cfc56a769
goldarai <46929210+goldarai@users.noreply.github.com>
2020-07-17 17:11:51 -0300

    Create 4_1_1_1.sh

---
+++ ubuntu-18.04/custom/4_1_1_1.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure auditd is installed (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=2
+cis_test_wpl=2
+
+function cis_test_run()
+{
+	cmd=$(dpkg -l auditd audisp-plugins | grep "no packages found")
+	[ -n "$cmd" ] && return 1
+    
+  	return 0
+}
