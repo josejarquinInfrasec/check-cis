1007df792527a4505b0d55196231defc7899719c
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-17 14:48:32 -0300

    -

--- ubuntu-18.04/custom/3_2_2.sh
+++ ubuntu-18.04/custom/3_2_2.sh
@@ -5,10 +5,10 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(sysctl net.ipv4.conf.all.accept_redirects 2>&1 | grep "cannot stat")
-	[ -z "$cmd" ] && return 1
-	cmd=$(sysctl net.ipv4.conf.default.accept_redirects 2>&1 | grep "cannot stat")
-	[ -z "$cmd" ] && return 1
+	cmd=$(sysctl net.ipv4.conf.all.accept_redirects)
+	[ "$cmd" != "net.ipv4.conf.all.accept_redirects = 0" ] && return 1
+	cmd=$(sysctl net.ipv4.conf.default.accept_redirects)
+	[ "$cmd" != "net.ipv4.conf.default.accept_redirects = 0" ] && return 1
 	cmd=$(grep "net\.ipv4\.conf\.all\.accept_redirects" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep -v "net.ipv4.conf.all.accept_redirects = 0")
 	[ -n "$cmd" ] && return 1
 	cmd=$(grep "net\.ipv4\.conf\.default\.accept_redirects" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep -v "net.ipv4.conf.default.accept_redirects = 0")
