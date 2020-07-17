2aee94c5e177f7d6ddb9f6a67136933458ffeaec
Maxi Fernandez <maxfernandez@despegar.com>
2020-07-17 17:08:33 -0300

    4_2_1_1.sh

---
+++ ubuntu-18.04/4_2_1_1.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure rsyslog is installed (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(dpkg -s rsyslog | grep -i "status: install ok installed")
+	[ -z "$cmd" ] && return 1
+
+	return 0
+}
