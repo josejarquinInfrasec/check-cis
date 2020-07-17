452662a5c214cd0416131c11c32b3a0c3c5d7976
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-17 17:13:56 -0300

    -

--- ubuntu-18.04/1_6_3.sh
+++ ubuntu-18.04/1_6_3.sh
@@ -1,4 +1,3 @@
-package_name="prelink"
 cis_test_name="Ensure prelink is disabled (Scored)"
 cis_test_pa=(server workstation)
 cis_test_spl=1
@@ -6,9 +5,9 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(dpkg -s ${package_name} 2>&1 | grep 'not installed')
+	cmd=$(dpkg -s prelink 2>&1 | grep 'not installed')
 	[ -z "$cmd" ] && return 1
-	
+
 	return 0
 }
 
--- ubuntu-18.04/1_7_1_2.sh
+++ ubuntu-18.04/1_7_1_2.sh
@@ -5,11 +5,11 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(dpkg -s apparmor apparmor-utils | grep -i '^status' | grep -vi 'status: install ok installed')
+	cmd=$(grep "^\s*linux" /boot/grub/grub.cfg | grep -v "apparmor=1" | grep -v '/boot/memtest86+.bin')
 	[ -n "$cmd" ] && return 1
 
 	cmd=$(grep "^\s*linux" /boot/grub/grub.cfg | grep -v "security=apparmor" | grep -v '/boot/memtest86+.bin')
 	[ -n "$cmd" ] && return 1
-	
+
 	return 0
 }
