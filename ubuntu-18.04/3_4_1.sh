ddf2512cf4a5de6a7a306ce8eb7b5e7752f825ed
goldarai <46929210+goldarai@users.noreply.github.com>
2020-07-17 15:54:47 -0300

    Create 3_4_1.sh

---
+++ ubuntu-18.04/3_4_1.sh
@@ -0,0 +1,15 @@
+cis_test_name=" Ensure DCCP is disabled (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=2
+cis_test_wpl=2
+
+function cis_test_run()
+{
+	cmd=$(modprobe -n -v dccp)
+	[ "$cmd" != "install /bin/true" ] && return 1
+    
+	cmd=$(lsmod | grep dccp)
+	[ -n "$cmd" ] && return 1
+	
+  	return 0
+}
