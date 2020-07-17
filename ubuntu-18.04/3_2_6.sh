af874e06b7416f2df6f987ff2df5816d0e3ab9e9
Maxi Fernandez <maxfernandez@despegar.com>
2020-07-17 14:53:15 -0300

    3_2_6

---
+++ ubuntu-18.04/3_2_6.sh
@@ -0,0 +1,13 @@
+cis_test_name="Ensure source routed packets are not accepted (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(sysctl net.ipv4.icmp_ignore_bogus_error_responses)
+	[ "$cmd" != "net.ipv4.icmp_ignore_bogus_error_responses = 1" ] && return 1
+	cmd=$(grep "net.ipv4.icmp_ignore_bogus_error_responses" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep "net.ipv4.icmp_ignore_bogus_error_responses = 1" | wc -l)
+	[ $cmd -ne 2 ] && return 1
+	return 0
+}
