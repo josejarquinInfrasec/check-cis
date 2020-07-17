4dc1ba84d8070b718b59723cb6c3dae2fcc1c767
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-16 21:59:12 -0300

    -

--- ubuntu-18.04/1_2_1.sh
+++ ubuntu-18.04/1_2_1.sh
@@ -1,7 +1,7 @@
 cis_test_name="Ensure package manager repositories are configured (Not Scored)"
 cis_test_pa=(server workstation)
 cis_test_spl=1
-cis_test_wpl=1apt-key list
+cis_test_wpl=1
 
 function cis_test_run()
 {
--- ubuntu-18.04/1_3_3.sh
+++ ubuntu-18.04/1_3_3.sh
@@ -5,7 +5,7 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(grep -Ei '^\s*Defaults\s+logfile=\S+' /etc/sudoers /etc/sud)
+	cmd=$(grep -Ei '^\s*Defaults\s+logfile=\S+' /etc/sudoers /etc/sudoers.d/*)
 	[ -z "$cmd" ] && return 1
 	return 0
 }
