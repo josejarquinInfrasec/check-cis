a1bb9644c672b7993fa5b95e2f166c976f0fb9df
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-22 15:22:51 -0300

    -

--- ubuntu-18.04/custom/4_1_1_1.sh
+++ ubuntu-18.04/custom/4_1_1_1.sh
@@ -5,7 +5,7 @@ cis_test_wpl=2
 
 function cis_test_run()
 {
-	cmd=$(dpkg -l auditd audisp-plugins 2>&1 | grep "no packages found")
+	cmd=$(dpkg -l auditd 2>&1 | grep "no packages found")
 	[ -n "$cmd" ] && return 1
     
   	return 0
