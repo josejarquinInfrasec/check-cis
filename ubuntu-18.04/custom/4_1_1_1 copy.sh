4ac67fdac9aa112ef167678a12f06bba1fea075d
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-22 15:21:40 -0300

    -

--- ubuntu-18.04/custom/4_1_1_1 copy.sh
+++ ubuntu-18.04/custom/4_1_1_1 copy.sh
@@ -5,7 +5,7 @@ cis_test_wpl=2
 
 function cis_test_run()
 {
-	cmd=$(dpkg -s auditd audispd-plugins | grep -i "^status" | grep -vi "status: install ok installed")
+	cmd=$(dpkg -s auditd | grep -i "^status" | grep -vi "status: install ok installed")
 	[ -n "$cmd" ] && return 1
     
   	return 0
