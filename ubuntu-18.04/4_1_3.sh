a953261489c43599758c53d702bde1bc6a9987ed
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-31 17:29:25 -0300

    -

--- ubuntu-18.04/4_1_3.sh
+++ ubuntu-18.04/4_1_3.sh
@@ -8,5 +8,8 @@ function cis_test_run()
 	cmd=$(grep time-change /etc/audit/rules.d/*.rules | md5sum)
 	[ "$cmd" != "db6a1675716a6a4b9e9c66adea415cfd  -" ] && return 1
 
+	cmd=$(auditctl -l | grep time-change | md5sum)
+	[ "$cmd" != "3a9d8b0f88e2da89d095dadcff097147  -" ] && return 1
+
 	return 0
 }
