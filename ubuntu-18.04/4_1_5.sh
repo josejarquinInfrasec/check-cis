e4aea1465d2dc4f8b6cbfbada632d6c2894df869
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-31 17:26:39 -0300

    -

--- ubuntu-18.04/4_1_5.sh
+++ ubuntu-18.04/4_1_5.sh
@@ -8,5 +8,8 @@ function cis_test_run()
 	cmd=$(grep system-locale /etc/audit/rules.d/*.rules | md5sum)
 	[ "$cmd" != "deacf10a883484b740051589471fb976  -" ] && return 1
 
+	cmd=$(auditctl -l | grep system-locale | md5sum)
+	[ "$cmd" != "ae393af93504c4c276c6a6e300649ff6  -" ] && return 1
+
 	return 0
 }
