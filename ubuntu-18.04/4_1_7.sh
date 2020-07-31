44f92b0f54c8528623016d6d0f3ad55c5da701a9
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-31 17:28:15 -0300

    -

--- ubuntu-18.04/4_1_7.sh
+++ ubuntu-18.04/4_1_7.sh
@@ -9,7 +9,7 @@ function cis_test_run()
 	[ "$cmd" != "951b08008f0bb667c2df815aee983db9  -" ] && return 1
 
 	cmd=$(auditctl -l | grep logins | md5sum)
-	[ "$cmd" != "1cae2a1bce415c00c7336f0de83a1a06  -" ] && return 1
+	[ "$cmd" != "951b08008f0bb667c2df815aee983db9  -" ] && return 1
 
 	return 0
 }
