782fceba145cfc98c7e404980957084ecd9a34c9
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-31 16:58:44 -0300

    -

---
+++ ubuntu-18.04/4_1_3.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure events that modify date and time information are collected (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=2
+cis_test_wpl=2
+
+function cis_test_run()
+{
+	cmd=$(grep time-change /etc/audit/rules.d/*.rules | md5sum)
+	[ "$cmd" != "db6a1675716a6a4b9e9c66adea415cfd  -" ] && return 1
+
+	return 0
+}
