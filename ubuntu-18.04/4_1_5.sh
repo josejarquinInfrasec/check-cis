13f343a4d2717e653fe332c6e541c05d27461b33
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-31 17:01:51 -0300

    -

---
+++ ubuntu-18.04/4_1_5.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure events that modify the system's network environment are collected (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=2
+cis_test_wpl=2
+
+function cis_test_run()
+{
+	cmd=$(grep system-locale /etc/audit/rules.d/*.rules | md5sum)
+	[ "$cmd" != "deacf10a883484b740051589471fb976  -" ] && return 1
+
+	return 0
+}
