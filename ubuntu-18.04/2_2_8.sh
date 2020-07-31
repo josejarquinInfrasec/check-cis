6e34656657356e9fb9df9644f2a6f3e602d8d2e2
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-31 16:00:25 -0300

    -

---
+++ ubuntu-18.04/2_2_8.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure DNS Server is not enabled (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(systemctl is-enabled bind9)
+	[ "$cmd" != "disabled" ] && return 1
+
+	return 0
+}
