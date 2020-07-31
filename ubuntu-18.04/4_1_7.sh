0d39d98b2497ad91440677b7a7e8c090701d536d
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-07-31 17:04:44 -0300

    -

---
+++ ubuntu-18.04/4_1_7.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure login and logout events are collected (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=2
+cis_test_wpl=2
+
+function cis_test_run()
+{
+	cmd=$(grep logins /etc/audit/rules.d/*.rules | md5sum)
+	[ "$cmd" != "951b08008f0bb667c2df815aee983db9  -" ] && return 1
+
+	return 0
+}
