2e2fbaea90d01347f17b7d7195a701ed3c29d600
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-03 15:10:47 -0300

    4.2.2.3

---
+++ ubuntu-18.04/4_2_2_3.sh
@@ -0,0 +1,13 @@
+cis_test_name="Ensure journald is configured to write logfiles to persistent disk
+(Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+    cmd=$(grep -E -i "^\s*Storage" /etc/systemd/journald.conf | grep "Storage=persistent")
+    [ -z "$cmd" ] && return 1
+
+    return 0
+}
