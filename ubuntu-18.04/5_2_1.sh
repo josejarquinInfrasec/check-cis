2d5d49818d10d9ec8c37b677cb28e711222d24ee
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-03 16:14:55 -0300

    5.2.1

---
+++ ubuntu-18.04/5_2_1.sh
@@ -0,0 +1,13 @@
+cis_test_name="Ensure permissions on /etc/ssh/sshd_config are configured
+(Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(stat /etc/ssh/sshd_config | grep 'Access: (0600/-rw-------)  Uid: (    0/    root)   Gid: (    0/    root)')
+	[ -z "$cmd" ] && return 1
+
+	return 0
+}
