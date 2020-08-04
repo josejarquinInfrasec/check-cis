56e28082ca253da4a5d1c03033f45f9469b1361f
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-08-04 14:54:09 -0300

    -

---
+++ ubuntu-18.04/5_2_7.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure SSH MaxAuthTries is set to 4 or less (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(sshd -T | grep "MaxAuthTries 4")
+	[ -z "$cmd" ] && return 1
+
+	return 0
+}
