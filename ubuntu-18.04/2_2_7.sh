a36451a796fc5e48e9300b39e6717244dc264598
Ignacio Goldaraz <nacho@AR2C4TST2.ar.infra.d>
2020-08-07 15:13:43 -0300

    Added 2_2_8.sh control

--- ubuntu-18.04/2_2_7.sh
+++ ubuntu-18.04/2_2_7.sh
@@ -1,11 +1,11 @@
-cis_test_name="Ensure HTTP Proxy Server is not enabled (Scored)"
+cis_test_name="Ensure NFS and RPC are not enabled (Scored)"
 cis_test_pa=(server workstation)
 cis_test_spl=1
 cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(systemctl is-enabled squid 2>&1)
+	cmd=$(systemctl is-enabled nfs-server 2>&1)
 	[ "$cmd" == "enabled" ] && return 1
 
 	return 0
