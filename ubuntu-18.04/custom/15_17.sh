3eabdd24e0358135a9585dd5c4b1e234ddd0cb70
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-10 16:18:31 -0300

    15.17

--- ubuntu-18.04/custom/15_17.sh
+++ ubuntu-18.04/custom/15_17.sh
@@ -1,11 +1,11 @@
-cis_test_name="Ensure SSH GatewayPorts is not configured"
+cis_test_name="Ensure SSH ignoreuserknownhosts is configured"
 cis_test_pa=(server workstation)
 cis_test_spl=1
 cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(sshd -T | grep "^gatewayports no")
+	cmd=$(sshd -T | grep "^ignoreuserknownhosts yes")
 	[ -z "$cmd" ] && return 1
 
 	return 0
