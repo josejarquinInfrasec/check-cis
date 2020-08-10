4135ace4f89e93835ae6456cd37d937e78035416
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-10 12:06:21 -0300

    comentarios

--- ubuntu-18.04/4_1_15.sh
+++ ubuntu-18.04/4_1_15.sh
@@ -5,9 +5,10 @@ cis_test_wpl=2
 
 function cis_test_run()
 {
+  # -w /var/log/sudo.log -p wa -k actions
 	cmd=$(grep actions /etc/audit/rules.d/*.rules | md5sum)
 	[ "$cmd" != "1cae2a1bce415c00c7336f0de83a1a06  -" ] && return 1
-
+  # -w /var/log/sudo.log -p wa -k actions
 	cmd=$(auditctl -l | grep actions | md5sum)
 	[ "$cmd" != "1cae2a1bce415c00c7336f0de83a1a06  -" ] && return 1
 
