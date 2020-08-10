da4b2b9374790b86eb3c56a62a29c76817eac815
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-10 12:11:49 -0300

    comentarios

--- ubuntu-18.04/4_1_4.sh
+++ ubuntu-18.04/4_1_4.sh
@@ -5,8 +5,18 @@ cis_test_wpl=2
 
 function cis_test_run()
 {
+    # -w /etc/group -p wa -k identity
+    # -w /etc/passwd -p wa -k identity
+    # -w /etc/gshadow -p wa -k identity
+    # -w /etc/shadow -p wa -k identity
+    # -w /etc/security/opasswd -p wa -k identity
     cmd=$(grep identity /etc/audit/rules.d/*.rules | md5sum)
     [ "$cmd" != "7a688dec39f25df61590759755aecbd5  -" ] && return 1
+    # -w /etc/group -p wa -k identity
+    # -w /etc/passwd -p wa -k identity
+    # -w /etc/gshadow -p wa -k identity
+    # -w /etc/shadow -p wa -k identity
+    # -w /etc/security/opasswd -p wa -k identity
     cmd=$(auditctl -l | grep identity | md5sum)
     [ "$cmd" != "7a688dec39f25df61590759755aecbd5  -" ] && return 1
 
