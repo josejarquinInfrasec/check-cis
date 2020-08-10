fb3b6bb0bf9c12d999d4e442f4c4e968fda5f705
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-10 12:24:31 -0300

    comentarios

--- ubuntu-18.04/6_1_14.sh
+++ ubuntu-18.04/6_1_14.sh
@@ -3,6 +3,25 @@ cis_test_pa=(server workstation)
 cis_test_spl=1
 cis_test_wpl=1
 
+#####################################################################################################
+#  df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000
+#####################################################################################################
+#  /sbin/unix_chkpwd
+#  /sbin/pam_extrausers_chkpwd
+#  /usr/lib/x86_64-linux-gnu/utempter/utempter
+#  /usr/bin/mlocate
+#  /usr/bin/at
+#  /usr/bin/expiry
+#  /usr/bin/mutt_dotlock
+#  /usr/bin/ssh-agent
+#  /usr/bin/wall
+#  /usr/bin/crontab
+#  /usr/bin/chage
+#  /usr/bin/bsd-write
+#  /usr/sbin/postqueue
+#  /usr/sbin/postdrop
+#####################################################################################################
+
 function cis_test_run()
 {
 	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | md5sum)
