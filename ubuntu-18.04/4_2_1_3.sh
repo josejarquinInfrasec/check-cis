caf0b628703a2513712bc56a6f81f5c3679409d3
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-10 12:20:50 -0300

    comentarios

--- ubuntu-18.04/4_2_1_3.sh
+++ ubuntu-18.04/4_2_1_3.sh
@@ -19,12 +19,6 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-  #  auth,authpriv.*			/var/log/auth.log
-  #  *.*;auth,authpriv.none		-/var/log/syslog
-  #  kern.*				-/var/log/kern.log
-  #  mail.*				-/var/log/mail.log
-  #  mail.err			/var/log/mail.err
-  #  *.emerg				:omusrmsg:*
 	cmd=$(grep -v '^#' /etc/rsyslog.d/50-default.conf | md5sum)
 	[ "$cmd" != "7359b6ef8f44a6846181976bd7d2c382  -" ] && return 1
 
--- ubuntu-18.04/6_1_13.sh
+++ ubuntu-18.04/6_1_13.sh
@@ -3,6 +3,34 @@ cis_test_pa=(server workstation)
 cis_test_spl=1
 cis_test_wpl=1
 
+################################################################
+#  /bin/ping
+#  /bin/fusermount
+#  /bin/su
+#  /bin/mount
+#  /bin/umount
+#  /usr/lib/x86_64-linux-gnu/lxc/lxc-user-nic
+#  /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+#  /usr/lib/openssh/ssh-keysign
+#  /usr/lib/policykit-1/polkit-agent-helper-1
+#  /usr/lib/eject/dmcrypt-get-device
+#  /usr/bin/pkexec
+#  /usr/bin/sudo
+#  /usr/bin/newgidmap
+#  /usr/bin/at
+#  /usr/bin/newgrp
+#  /usr/bin/gpasswd
+#  /usr/bin/chsh
+#  /usr/bin/chfn
+#  /usr/bin/traceroute6.iputils
+#  /usr/bin/newuidmap
+#  /usr/bin/passwd
+#  /opt/netdata/bin/fping
+#  /opt/netdata/usr/libexec/netdata/plugins.d/ioping
+#  /opt/netdata/usr/libexec/netdata/plugins.d/cgroup-network
+#  /opt/netdata/usr/libexec/netdata/plugins.d/apps.plugin
+################################################################
+
 function cis_test_run()
 {
 	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -4000 | md5sum)
