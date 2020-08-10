e4938262b4644a11081aa3e35399a6fa619b72de
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-10 12:17:37 -0300

    comentarios

--- ubuntu-18.04/4_1_7.sh
+++ ubuntu-18.04/4_1_7.sh
@@ -5,9 +5,14 @@ cis_test_wpl=2
 
 function cis_test_run()
 {
+  #  -w /var/log/faillog -p wa -k logins
+  #  -w /var/log/lastlog -p wa -k logins
+  #  -w /var/log/tallylog -p wa -k logins
 	cmd=$(grep logins /etc/audit/rules.d/*.rules | md5sum)
 	[ "$cmd" != "951b08008f0bb667c2df815aee983db9  -" ] && return 1
-
+  #  -w /var/log/faillog -p wa -k logins
+  #  -w /var/log/lastlog -p wa -k logins
+  #  -w /var/log/tallylog -p wa -k logins
 	cmd=$(auditctl -l | grep logins | md5sum)
 	[ "$cmd" != "951b08008f0bb667c2df815aee983db9  -" ] && return 1
 
