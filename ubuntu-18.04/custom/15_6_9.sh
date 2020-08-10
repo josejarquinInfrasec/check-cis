8ee95bb94f8ec88a4ef8d3581ba875aafab47123
Ignacio Goldaraz <nacho@AR2C4TST2.ar.infra.d>
2020-08-10 16:46:17 -0300

    Added 15_6_9.sh script

--- ubuntu-18.04/custom/15_6_9.sh
+++ ubuntu-18.04/custom/15_6_9.sh
@@ -1,11 +1,11 @@
-cis_test_name="Group despegar in user despegar"
+cis_test_name="Ensure umask 027 is configured"
 cis_test_pa=(server workstation)
 cis_test_spl=1
 cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(/usr/bin/groups despegar | grep "despegar : despegar")
+	cmd=$(grep 'UMASK   027' /etc/login.defs)
 	[ -z "$cmd" ] && return 1
 
 	return 0
