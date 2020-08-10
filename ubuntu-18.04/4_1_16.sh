2a76156e043d8d5c0f2ab0e9c6a04959cd5405d3
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-10 12:07:48 -0300

    comentarios

--- ubuntu-18.04/4_1_16.sh
+++ ubuntu-18.04/4_1_16.sh
@@ -5,8 +5,16 @@ cis_test_wpl=2
 
 function cis_test_run()
 {
+    # -w /sbin/insmod -p x -k modules
+    # -w /sbin/rmmod -p x -k modules
+    # -w /sbin/modprobe -p x -k modules
+    # -a always,exit -F arch=b64 -S init_module -S delete_module -k modules
     cmd=$(grep modules /etc/audit/rules.d/*.rules | md5sum)
     [ "$cmd" != "cd1e7a1cfc749edc93d35126060b0a42  -" ] && return 1
+    # -w /sbin/insmod -p x -k modules
+    # -w /sbin/rmmod -p x -k modules
+    # -w /sbin/modprobe -p x -k modules
+    # -a always,exit -F arch=b64 -S init_module,delete_module -F key=modules
     cmd=$(auditctl -l | grep modules | md5sum)
     [ "$cmd" != "5fe80b2ba6323c886fe04ddfcb580f33  -" ] && return 1
 
