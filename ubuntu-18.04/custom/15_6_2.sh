57a7e66e82c1a4923532fe348003acb369b8f675
Ignacio Goldaraz <nacho@AR2C4TST2.ar.infra.d>
2020-08-10 16:02:44 -0300

    Added 15_6_2.sh script

---
+++ ubuntu-18.04/custom/15_6_2.sh
@@ -0,0 +1,27 @@
+cis_test_name="Ensure permissions on certain log files are configured"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(stat /var/log/mcollective.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+	[ -z "$cmd" ] && return 1
+
+	cmd=$(stat /var/log/cloud-init.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+	[ -z "$cmd" ] && return 1
+
+	cmd=$(stat /var/log/boot.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+	[ -z "$cmd" ] && return 1
+
+	cmd=$(stat /var/log/apt/history.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+	[ -z "$cmd" ] && return 1
+
+	cmd=$(stat /var/log/munin/munin-node.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+	[ -z "$cmd" ] && return 1
+
+	cmd=$(stat /var/log/dpkg.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+	[ -z "$cmd" ] && return 1
+
+	return 0
+}
