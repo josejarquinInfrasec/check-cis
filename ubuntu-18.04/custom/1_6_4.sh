a9030eb4c1b2ac196f9d3122c09b9b831fc95976
Ignacio Goldaraz <nacho@AR2C4TST2.ar.infra.d>
2020-08-11 12:31:20 -0300

    Fixed 1_6_4.sh custom script

--- ubuntu-18.04/custom/1_6_4.sh
+++ ubuntu-18.04/custom/1_6_4.sh
@@ -9,7 +9,7 @@ function cis_test_run()
 	[ -n "$cmd" ] && return 1
 
 	cmd=$(sysctl fs.suid_dumpable)
-	[ -z "$cmd" != "fs.suid_dumpable = 0" ] && return 1
+	[ "$cmd" != "fs.suid_dumpable = 0" ] && return 1
 
 	cmd=$(grep "fs\.suid_dumpable" /etc/sysctl.conf /etc/sysctl.d/* | grep -v 'fs.suid_dumpable = 0')
 	[ -n "$cmd" ] && return 1
