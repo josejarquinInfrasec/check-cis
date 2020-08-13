faea75a6a423008bccbeadcd8082042c922bc8e8
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-08-13 14:42:54 -0300

    -

--- ubuntu-18.04/custom/15_6_2.sh
+++ ubuntu-18.04/custom/15_6_2.sh
@@ -5,20 +5,33 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(stat /var/log/puppetlabs/mcollective/mcollective.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
-	[ -z "$cmd" ] && return 1
+	cloudiaguest=$(grep -Po '(?<=cloudiaguest:\s)\w+' /etc/puppetlabs/mcollective/generated-facts.yaml)
 
-	cmd=$(stat /var/log/cloud-init.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
-	[ -z "$cmd" ] && return 1
+	if [ "$cloudiaguest" == "kvm" ]; then
+		cmd=$(stat /var/log/puppetlabs/mcollective/mcollective.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
 
-	cmd=$(stat /var/log/apt/history.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
-	[ -z "$cmd" ] && return 1
+		cmd=$(stat /var/log/cloud-init.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
 
-	cmd=$(stat /var/log/munin/munin-node.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
-	[ -z "$cmd" ] && return 1
+		cmd=$(stat /var/log/apt/history.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
 
-	cmd=$(stat /var/log/dpkg.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
-	[ -z "$cmd" ] && return 1
+		cmd=$(stat /var/log/munin/munin-node.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
+
+		cmd=$(stat /var/log/dpkg.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
+	else
+		cmd=$(stat /var/log/apt/history.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
+
+		cmd=$(stat /var/log/munin/munin-node.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
+
+		cmd=$(stat /var/log/dpkg.log | grep 'Access: (0640/-rw-r-----)  Uid: (    0/    root)   Gid: (    0/    root)')
+		[ -z "$cmd" ] && return 1
+	fi
 
 	return 0
 }
