23ddfe657bd8354ffb7b8e1451f06c3b481cd164
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-08-13 14:31:34 -0300

    -

--- ubuntu-18.04/6_1_13.sh
+++ ubuntu-18.04/6_1_13.sh
@@ -4,37 +4,72 @@ cis_test_spl=1
 cis_test_wpl=1
 
 ################################################################
-#  /bin/ping
-#  /bin/fusermount
-#  /bin/su
-#  /bin/mount
-#  /bin/umount
-#  /usr/lib/x86_64-linux-gnu/lxc/lxc-user-nic
-#  /usr/lib/dbus-1.0/dbus-daemon-launch-helper
-#  /usr/lib/openssh/ssh-keysign
-#  /usr/lib/policykit-1/polkit-agent-helper-1
-#  /usr/lib/eject/dmcrypt-get-device
-#  /usr/bin/pkexec
-#  /usr/bin/sudo
-#  /usr/bin/newgidmap
-#  /usr/bin/at
-#  /usr/bin/newgrp
-#  /usr/bin/gpasswd
-#  /usr/bin/chsh
-#  /usr/bin/chfn
-#  /usr/bin/traceroute6.iputils
-#  /usr/bin/newuidmap
-#  /usr/bin/passwd
-#  /opt/netdata/bin/fping
-#  /opt/netdata/usr/libexec/netdata/plugins.d/ioping
-#  /opt/netdata/usr/libexec/netdata/plugins.d/cgroup-network
-#  /opt/netdata/usr/libexec/netdata/plugins.d/apps.plugin
+# kvm:
+# /bin/ping
+# /bin/fusermount
+# /bin/su
+# /bin/mount
+# /bin/umount
+# /usr/lib/x86_64-linux-gnu/lxc/lxc-user-nic
+# /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+# /usr/lib/openssh/ssh-keysign
+# /usr/lib/policykit-1/polkit-agent-helper-1
+# /usr/lib/eject/dmcrypt-get-device
+# /usr/bin/pkexec
+# /usr/bin/sudo
+# /usr/bin/newgidmap
+# /usr/bin/at
+# /usr/bin/newgrp
+# /usr/bin/gpasswd
+# /usr/bin/chsh
+# /usr/bin/chfn
+# /usr/bin/traceroute6.iputils
+# /usr/bin/newuidmap
+# /usr/bin/passwd
+# /opt/netdata/bin/fping
+# /opt/netdata/usr/libexec/netdata/plugins.d/ioping
+# /opt/netdata/usr/libexec/netdata/plugins.d/cgroup-network
+# /opt/netdata/usr/libexec/netdata/plugins.d/apps.plugi
+# physical:
+# /usr/lib/openssh/ssh-keysign
+# /usr/lib/policykit-1/polkit-agent-helper-1
+# /usr/lib/eject/dmcrypt-get-device
+# /usr/lib/x86_64-linux-gnu/lxc/lxc-user-nic
+# /usr/lib/dbus-1.0/dbus-daemon-launch-helper
+# /usr/bin/ksu
+# /usr/bin/pkexec
+# /usr/bin/chsh
+# /usr/bin/newgrp
+# /usr/bin/traceroute6.iputils
+# /usr/bin/newuidmap
+# /usr/bin/passwd
+# /usr/bin/gpasswd
+# /usr/bin/newgidmap
+# /usr/bin/sudo
+# /usr/bin/at
+# /usr/bin/chfn
+# /opt/netdata/usr/libexec/netdata/plugins.d/ioping
+# /opt/netdata/usr/libexec/netdata/plugins.d/apps.plugin
+# /opt/netdata/usr/libexec/netdata/plugins.d/cgroup-network
+# /opt/netdata/bin/fping
+# /bin/su
+# /bin/mount
+# /bin/umount
+# /bin/ping
+# /bin/fusermount
 ################################################################
 
 function cis_test_run()
 {
-	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -4000 | md5sum)
-	[ "$cmd" != "6c5ce88740e397ff4ba13e3ea971285e  -" ] && return 1
-
+	cloudiaguest=$(grep -Po '(?<=cloudiaguest:\s)\w+' /etc/puppetlabs/mcollective/generated-facts.yaml)
+	
+	if [ "$cloudiaguest" == "kvm" ]; then
+		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -4000 | md5sum)
+		[ "$cmd" != "41490c584644f60330e9a26e0e9d6055  -" ] && return 1
+	then
+		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -4000 | md5sum)
+		[ "$cmd" != "6c5ce88740e397ff4ba13e3ea971285e  -" ] && return 1
+	fi
+	
 	return 0
 }
--- ubuntu-18.04/6_1_14.sh
+++ ubuntu-18.04/6_1_14.sh
@@ -6,26 +6,46 @@ cis_test_wpl=1
 #####################################################################################################
 #  df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000
 #####################################################################################################
-#  /sbin/unix_chkpwd
-#  /sbin/pam_extrausers_chkpwd
-#  /usr/lib/x86_64-linux-gnu/utempter/utempter
-#  /usr/bin/mlocate
-#  /usr/bin/at
-#  /usr/bin/expiry
-#  /usr/bin/mutt_dotlock
-#  /usr/bin/ssh-agent
-#  /usr/bin/wall
-#  /usr/bin/crontab
-#  /usr/bin/chage
-#  /usr/bin/bsd-write
-#  /usr/sbin/postqueue
-#  /usr/sbin/postdrop
+# kvm:
+# /sbin/unix_chkpwd
+# /sbin/pam_extrausers_chkpwd
+# /usr/lib/x86_64-linux-gnu/utempter/utempter
+# /usr/bin/mlocate
+# /usr/bin/at
+# /usr/bin/expiry
+# /usr/bin/mutt_dotlock
+# /usr/bin/ssh-agent
+# /usr/bin/wall
+# /usr/bin/crontab
+# /usr/bin/chage
+# /usr/bin/bsd-write
+# /usr/sbin/postqueue
+# /usr/sbin/postdrop
+# physical:
+# /usr/lib/x86_64-linux-gnu/utempter/utempter
+# /usr/bin/crontab
+# /usr/bin/bsd-write
+# /usr/bin/chage
+# /usr/bin/ssh-agent
+# /usr/bin/wall
+# /usr/bin/mlocate
+# /usr/bin/expiry
+# /usr/bin/at
+# /sbin/pam_extrausers_chkpwd
+# /sbin/unix_chkpwd
 #####################################################################################################
 
 function cis_test_run()
 {
-	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | md5sum)
-	[ "$cmd" != "826ff09701a75045036aa001115e3790  -" ] && return 1
+	cloudiaguest=$(grep -Po '(?<=cloudiaguest:\s)\w+' /etc/puppetlabs/mcollective/generated-facts.yaml)
+
+	if [ "$cloudiaguest" == "kvm" ]; then
+		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | md5sum)
+		[ "$cmd" != "826ff09701a75045036aa001115e3790  -" ] && return 1
+	else
+		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | md5sum)
+		[ "$cmd" != "2654b703cf4d8120fe03f19fdfa604dc  -" ] && return 1
+	fi
 
 	return 0
 }
