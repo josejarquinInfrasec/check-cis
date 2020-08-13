1204fceeef4694d6f4ef20108bf54354862c0ec7
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-08-13 14:45:50 -0300

    -

--- ubuntu-18.04/custom/15_29.sh
+++ ubuntu-18.04/custom/15_29.sh
@@ -5,12 +5,19 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
+  cloudiaguest=$(grep -Po '(?<=cloudiaguest:\s)\w+' /etc/puppetlabs/mcollective/generated-facts.yaml)
+
   host_ip=$(ifconfig ens3 | grep -Po '(?<=inet\s)[\.\d]+')
   cmd=$(grep -P "^\d+\s[\w\-]+\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
   [ -z "$cmd" ] && return 1
 
-  cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
-  [ "$cmd" != "eef3cc10a70e78d68a505f0a380504df  -" ] && return 1
+  if [ "$cloudiaguest" == "kvm" ]; then
+    cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
+    [ "$cmd" != "eef3cc10a70e78d68a505f0a380504df  -" ] && return 1
+  else
+    cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
+    [ "$cmd" != "7f4402caf577bcd20949e3c5226611ab  -" ] && return 1
+  fi
 
   return 0
 }
