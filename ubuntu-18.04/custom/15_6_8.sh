9cfe14793fa9ddb23c4d2bf9317816d4a491f1dc
elarrarte-d <49532154+elarrarte-d@users.noreply.github.com>
2020-08-18 19:04:32 -0300

    Update 15_6_8.sh

--- ubuntu-18.04/custom/15_6_8.sh
+++ ubuntu-18.04/custom/15_6_8.sh
@@ -1,11 +1,11 @@
-cis_test_name="Group despegar in user despegar"
+cis_test_name="Ensure user despegar is not in group adm"
 cis_test_pa=(server workstation)
 cis_test_spl=1
 cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(/usr/bin/groups despegar | grep "despegar : despegar")
+	cmd=$(/usr/bin/groups despegar | grep -P "\sadm\s?")
 	[ -z "$cmd" ] && return 1
 
 	return 0
