7df18a3402196ccb3237dd4577b25a025a198d97
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-19 14:45:15 -0300

    corrección 3.2.9

--- ubuntu-18.04/custom/3_2_9.sh
+++ ubuntu-18.04/custom/3_2_9.sh
@@ -5,16 +5,16 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(sysctl net.ipv6.conf.all.accept_ra 2>&1 | grep -v "net.ipv6.conf.all.accept_ra\s*=\s*0")
-	[ -z "$cmd" ] && return 1
+  cmd=$(sysctl net.ipv6.conf.all.accept_ra 2>&1)
+    [ "$cmd" != "net.ipv6.conf.all.accept_ra = 0" -a -n "$(echo $cmd | grep -v 'cannot stat')" ] && return 1
 
-	cmd=$(sysctl net.ipv6.conf.default.accept_ra 2>&1 | grep "cannot stat")
-	[ -z "$cmd" ] && return 1
+  cmd=$(sysctl net.ipv6.conf.default.accept_ra 2>&1)
+    [ "$cmd" != "net.ipv6.conf.default.accept_ra = 0" -a -n "$(echo $cmd | grep -v 'cannot stat')" ] && return 1
 
-	cmd=$(grep "net\.ipv6\.conf\.all\.accept_ra" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep "net.ipv6.conf.all.accept_ra = 0" | wc -l)
+	cmd=$(grep "net\.ipv6\.conf\.all\.accept_ra" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep "net.ipv6.conf.all.accept_ra\s*=\s*0" | wc -l)
 	[ $cmd -ne 2 ] && return 1
 
-	cmd=$(grep "net\.ipv6\.conf\.default\.accept_ra" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep "net.ipv6.conf.default.accept_ra = 0" | wc -l)
+	cmd=$(grep "net\.ipv6\.conf\.default\.accept_ra" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep "net.ipv6.conf.default.accept_ra\s*=\s*0" | wc -l)
 	[ $cmd -ne 2 ] && return 1
 
 	return 0
