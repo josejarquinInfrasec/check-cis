d59fe577f9d034e43ba63829b6967eab3fcd1f20
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-19 14:07:29 -0300

    corrección 6.1.14

--- ubuntu-18.04/6_1_14.sh
+++ ubuntu-18.04/6_1_14.sh
@@ -7,32 +7,20 @@ cis_test_wpl=1
 #  df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000
 #####################################################################################################
 # kvm:
-# /sbin/unix_chkpwd
-# /sbin/pam_extrausers_chkpwd
-# /usr/lib/x86_64-linux-gnu/utempter/utempter
-# /usr/bin/mlocate
-# /usr/bin/at
-# /usr/bin/expiry
-# /usr/bin/mutt_dotlock
-# /usr/bin/ssh-agent
-# /usr/bin/wall
-# /usr/bin/crontab
-# /usr/bin/chage
-# /usr/bin/bsd-write
-# /usr/sbin/postqueue
-# /usr/sbin/postdrop
-# physical:
-# /usr/lib/x86_64-linux-gnu/utempter/utempter
-# /usr/bin/crontab
-# /usr/bin/bsd-write
-# /usr/bin/chage
-# /usr/bin/ssh-agent
-# /usr/bin/wall
-# /usr/bin/mlocate
-# /usr/bin/expiry
-# /usr/bin/at
-# /sbin/pam_extrausers_chkpwd
-# /sbin/unix_chkpwd
+#/sbin/pam_extrausers_chkpwd
+#/sbin/unix_chkpwd
+#/usr/bin/at
+#/usr/bin/bsd-write
+#/usr/bin/chage
+#/usr/bin/crontab
+#/usr/bin/expiry
+#/usr/bin/mlocate
+#/usr/bin/mutt_dotlock
+#/usr/bin/ssh-agent
+#/usr/bin/wall
+#/usr/lib/x86_64-linux-gnu/utempter/utempter
+#/usr/sbin/postdrop
+#/usr/sbin/postqueue
 #####################################################################################################
 
 function cis_test_run()
@@ -40,11 +28,11 @@ function cis_test_run()
 	cloudiaguest=$(grep -Po '(?<=cloudiaguest:\s)\w+' /etc/puppetlabs/mcollective/generated-facts.yaml)
 
 	if [ "$cloudiaguest" == "kvm" ]; then
-		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | md5sum)
-		[ "$cmd" != "826ff09701a75045036aa001115e3790  -" ] && return 1
+		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | sort | md5sum)
+		[ "$cmd" != "2bca50da59920b937deb66e7b6aa9f78  -" ] && return 1
 	else
-		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | md5sum)
-		[ "$cmd" != "2654b703cf4d8120fe03f19fdfa604dc  -" ] && return 1
+		cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -2000 | sort | md5sum)
+		[ "$cmd" != "2bca50da59920b937deb66e7b6aa9f78  -" ] && return 1
 	fi
 
 	return 0
