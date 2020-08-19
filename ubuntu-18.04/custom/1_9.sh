28962d8919a1ce07632b7acffb3b5fbe5d9bc3d9
elarrarte-d <49532154+elarrarte-d@users.noreply.github.com>
2020-08-19 08:25:38 -0300

    Updated 1_9

--- ubuntu-18.04/custom/1_9.sh
+++ ubuntu-18.04/custom/1_9.sh
@@ -5,6 +5,9 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
+	# Ya que Puppet aplica las actualizaciones en forma progresiva a lo largo de todo el ambiente PCI, antes de ejecutar este test en una instancia, se debe ejecutar:
+	# apt update
+	# unattended-upgrade -d (el mismo comando que ejecuta Puppet)
 	cmd=$(apt -s dist-upgrade 2>&1 | grep '^Inst' | grep "/$(lsb_release -cs)-security" | wc -l)
 	[ $cmd -gt 0 ] && return 1
 	
