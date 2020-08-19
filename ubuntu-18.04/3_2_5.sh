7e84c2e6f9cec6e6fea292a1bc6935dfab66f033
Maxi Fernandez <maxfernandez@despegar.com>
2020-08-19 14:49:36 -0300

    corrección 3.2.5

--- ubuntu-18.04/3_2_5.sh
+++ ubuntu-18.04/3_2_5.sh
@@ -8,7 +8,7 @@ function cis_test_run()
 	cmd=$(sysctl net.ipv4.icmp_echo_ignore_broadcasts)
 	[ "$cmd" != "net.ipv4.icmp_echo_ignore_broadcasts = 1" ] && return 1
 
-	cmd=$(grep "net\.ipv4\.icmp_echo_ignore_broadcasts" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep -v "net.ipv4.icmp_echo_ignore_broadcasts = 1")
+	cmd=$(grep "net\.ipv4\.icmp_echo_ignore_broadcasts" /etc/sysctl.conf /etc/sysctl.d/* | grep -v "#" | grep -v "net.ipv4.icmp_echo_ignore_broadcasts\s*=\s*1")
 	[ -n "$cmd" ] && return 1
 	
 	return 0
