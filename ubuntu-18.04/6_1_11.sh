fc5e9df65b7ba846d46c8ccb8c51422334580afb
elarrarte-d <49532154+elarrarte-d@users.noreply.github.com>
2020-08-19 11:19:18 -0300

    Update 6_1_11.sh

--- ubuntu-18.04/6_1_11.sh
+++ ubuntu-18.04/6_1_11.sh
@@ -7,6 +7,8 @@ function cis_test_run()
 {
 	# Se exceptuan los archivos de systemd, confirmados por el equipo de Cloud
 	# https://www.buihanotes.com/2019/05/technical-memo-dynamicuser-in-systemd.html
+	# /var/lib/private/systemd/timesync
+	# /var/lib/private/systemd/timesync/clock
 	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -nouser | grep -v "/var/lib/private/systemd")
 	[ -n "$cmd" ] && return 1
 
