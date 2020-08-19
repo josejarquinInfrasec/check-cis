fe2162a6c0ce950f2c2563d22fe42fbe29d7510e
elarrarte-d <49532154+elarrarte-d@users.noreply.github.com>
2020-08-19 11:16:10 -0300

    Update 5_4_2.sh

--- ubuntu-18.04/custom/5_4_2.sh
+++ ubuntu-18.04/custom/5_4_2.sh
@@ -5,6 +5,7 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
+	# Se exceptua el usuario munin-async (kvm) y netdata (physical)
 	cmd=$(awk -F: '($1!="root" && $1!="sync" && $1!="shutdown" && $1!="halt" && $1!~/^\+/ && $3<'"$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)"' && $7!="'"$(which nologin)"'" && $7!="/bin/false") {print}' /etc/passwd | grep -vP '(munin-async|netdata)')
 	[ -n "$cmd" ] && return 1
 
