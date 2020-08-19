a4d3da77b21e9508c1803bafb52547e1a9023e46
elarrarte-d <49532154+elarrarte-d@users.noreply.github.com>
2020-08-19 08:18:39 -0300

    Updated 6_1_10

--- ubuntu-18.04/6_1_10.sh
+++ ubuntu-18.04/6_1_10.sh
@@ -5,7 +5,8 @@ cis_test_wpl=1
 
 function cis_test_run()
 {
-	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -0002)
+	# Se execptua JVM de Amazon, el paquete ya incluye archivos con permisos 777 correspondientes a informacion de licencia
+	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -0002 | grep -vP "/usr/lib/jvm/java-\d+-amazon")
 	[ -n "$cmd" ] && return 1
 
 	return 0
