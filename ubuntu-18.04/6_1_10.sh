7488df92573933e59df3d54b65cf7813b3e6d5da
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-08-19 12:16:16 -0300

    -

--- ubuntu-18.04/6_1_10.sh
+++ ubuntu-18.04/6_1_10.sh
@@ -6,6 +6,7 @@ cis_test_wpl=1
 function cis_test_run()
 {
 	# Se execptua JVM de Amazon, el paquete ya incluye archivos con permisos 777 correspondientes a informacion de licencia
+	# /usr/lib/jvm-XX-amazon
 	cmd=$(df --local -P | awk '{if (NR!=1) print $6}' | xargs -I '{}' find '{}' -xdev -type f -perm -0002 | grep -vP "/usr/lib/jvm/java-\d+-amazon")
 	[ -n "$cmd" ] && return 1
 
