85f96e56f58d3a410f78d1e9871b4e83f9955f56
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-08-25 13:11:45 -0300

    -

--- ubuntu-18.04/custom/15_29.sh
+++ ubuntu-18.04/custom/15_29.sh
@@ -8,23 +8,38 @@ function cis_test_run()
   if [[ "$(hostname)" != "soc-wazuh-mg-"* ]]; then
     #cloudiaguest=$(grep -Po '(?<=cloudiaguest:\s)\w+' /etc/puppetlabs/mcollective/generated-facts.yaml)
     cloudiaguest=$(facter -p cloudiaguest)
+    environment=$(facter -p environment | grep -Po "^(pci|prod)")
 
-    if [ "$cloudiaguest" == "kvm" ]; then
-      host_ip=$(ifconfig ens3 | grep -Po '(?<=inet\s)[\.\d]+')
-      cmd=$(grep -P "^\d+\s$(hostname)\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
-      [ -z "$cmd" ] && return 1
+    case "$environment" in
+      pci)
+        if [ "$cloudiaguest" == "kvm" ]; then
+          host_ip=$(ifconfig ens3 | grep -Po '(?<=inet\s)[\.\d]+')
+          cmd=$(grep -P "^\d+\s$(hostname)\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
+          [ -z "$cmd" ] && return 1
 
-      cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
-      [ "$cmd" != "eef3cc10a70e78d68a505f0a380504df  -" ] && return 1
-    else
-      host_ip=$(ifconfig bond0 | grep -Po '(?<=inet\s)[\.\d]+')
-      cmd=$(grep -P "^\d+\s$(hostname)\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
-      [ -z "$cmd" ] && return 1
+          cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
+          [ "$cmd" != "eef3cc10a70e78d68a505f0a380504df  -" ] && return 1
+        else
+          host_ip=$(ifconfig bond0 | grep -Po '(?<=inet\s)[\.\d]+')
+          cmd=$(grep -P "^\d+\s$(hostname)\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
+          [ -z "$cmd" ] && return 1
 
-      cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
-      [ "$cmd" != "7f4402caf577bcd20949e3c5226611ab  -" ] && return 1
-    fi
-  fi
+          cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
+          [ "$cmd" != "7f4402caf577bcd20949e3c5226611ab  -" ] && return 1
+        fi
+      ;;
+
+      prod)
+        if [ "$cloudiaguest" == "kvm" ]; then
+          host_ip=$(ifconfig ens3 | grep -Po '(?<=inet\s)[\.\d]+')
+          cmd=$(grep -P "^\d+\s$(hostname)\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
+          [ -z "$cmd" ] && return 1
+
+          cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
+          [ "$cmd" != "9eb74877895a5d982dfbe825651b7a91  -" ] && return 1
+        fi
+      ;;
+    esac
 
   return 0
 }
