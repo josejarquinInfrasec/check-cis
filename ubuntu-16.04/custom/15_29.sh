8f203b5156c0b13a1349cda536ccecf2c532573e
Ezequiel Larrarte <elarrarte@ARCDDHVT2.localdomain>
2020-08-25 13:24:17 -0300

    -

--- ubuntu-16.04/custom/15_29.sh
+++ ubuntu-16.04/custom/15_29.sh
@@ -8,9 +8,11 @@ function cis_test_run()
   if [[ "$(hostname)" != "soc-wazuh-mg-"* ]]; then
     #cloudiaguest=$(grep -Po '(?<=cloudiaguest:\s)\w+' /etc/puppetlabs/mcollective/generated-facts.yaml)
     cloudiaguest=$(facter -p cloudiaguest)
-    environment=$(facter -p environment | grep -Po "^(pci|prod)")
+    environment=$(facter -p environment)
+    environment_name=$(echo $environment | grep -Po "^(pci|prod)")
+    environment_openstack=$(echo $environment | grep -Po "(mitaka|queens)$")
 
-    case "$environment" in
+    case "$environment_name" in
       pci)
         if [ "$cloudiaguest" == "kvm" ]; then
           host_ip=$(ifconfig ens3 | grep -Po '(?<=inet\s)[\.\d]+')
@@ -31,16 +33,18 @@ function cis_test_run()
 
       prod)
         if [ "$cloudiaguest" == "kvm" ]; then
-          host_ip=$(ifconfig ens3 | grep -Po '(?<=inet\s)[\.\d]+')
-          cmd=$(grep -P "^\d+\s$(hostname)\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
-          [ -z "$cmd" ] && return 1
+          if [ "$environment_openstack" == "queens" ]; then
+            host_ip=$(ifconfig ens3 | grep -Po '(?<=inet\s)[\.\d]+')
+            cmd=$(grep -P "^\d+\s$(hostname)\s${host_ip}\s.{64}" /var/ossec/etc/client.keys)
+            [ -z "$cmd" ] && return 1
+          fi
 
           cmd=$(cat /var/ossec/etc/shared/agent.conf | md5sum)
           [ "$cmd" != "9eb74877895a5d982dfbe825651b7a91  -" ] && return 1
         fi
       ;;
     esac
+  fi
 
   return 0
 }
-
