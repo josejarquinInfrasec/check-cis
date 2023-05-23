70cf50aa562e1b9c630e7c6892c07fa8b6eb5ef7
smusto-despegar <94704157+smusto-despegar@users.noreply.github.com>
2023-05-23 10:12:10 -0300

    Create 1_1_1_6.sh

---
+++ ubuntu-20.04/custom/1_1_1_6.sh
@@ -0,0 +1,12 @@
+cis_test_name="Ensure mounting of squashfs filesystems is disabled (Scored)"
+cis_test_pa=(server workstation)
+cis_test_spl=1
+cis_test_wpl=1
+
+function cis_test_run()
+{
+	cmd=$(grep "install squashfs /bin/true" /etc/modprobe.d/CIS-squashfs.conf)
+	[ -z "$cmd" ] && return 1
+	
+	return 0
+}
